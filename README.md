f9e67bc8c9b80e2759f8f8dd201efd8568ab0d77
Keith Burns (alagalah) <alagalah@gmail.com>
2018-01-19 09:26:03 -0800

    Addressed following TODOs: - removed hardcoding of Envoy src path -- added env var ENVOY_SRC - add bazel generated cache dir to GDB directories
    Signed-off-by: Keith Burns (alagalah) <alagalah@gmail.com>

--- README.md
+++ README.md
@@ -2,14 +2,12 @@
 
 I like emacs and gud-gdb.
 
-WARNING: has my local paths hard-coded in places (see TODO).
-
 
 From local clone of this repo:
 
 ```bash
- export FDIO=/git/work/fdio
- ./tools/bazel-test-gdb --package_path %workspace%:$FDIO/envoy //test/common/http:async_client_impl_test --test_env=ENVOY_IP_TEST_VERSIONS=v4only -c dbg
+ export ENVOY_SRC=/git/work/fdio/envoy
+ ./tools/bazel-test-gdb --package_path %workspace%:$ENVOY_SRC //test/common/http:async_client_impl_test --test_env=ENVOY_IP_TEST_VERSIONS=v4only -c dbg
 ```
 
 Does following:
@@ -21,10 +19,4 @@ Does following:
 For debugging UT from github.com/alagalah/envoy
 
 ## TODO
-* "set directories" in .py needs to have the ~/.cache/bazel/.../<UUID> added in order to step through source
-  * temporarily you can, for example:
-```
-(gdb) directory  /home/alagalah/.cache/bazel/_bazel_alagalah/b3ac823edb50659b90c8bc3c2a07ed24
-```
-* gtags/ctags has an issue where tags can't be generated due to some perms issue. Ensure gtags up to date prior to running UT
-* get rid of hard coded path references to my local repo. Only did this because all the nested / escaping was making me crazy
+* gtags/ctags has an issue where tags can't be generated from the emacs instance launched from debugger. Could be perms (??) but generating tags prior to debug is way to go
--- tools/gen_gdb_wrapper_script.py
+++ tools/gen_gdb_wrapper_script.py
@@ -19,30 +19,35 @@ f = open(fn,'w')
 
 env = ${b64env}
 env_lines = []
-env_lines.append("set directories /git/work/fdio/envoy \\n")
 for k, v in env.iteritems():
   os.environ[k] = v
   if not k.startswith('BASH_FUNC'):
     env_lines.append("set environment "+str(k)+"="+str(v)+"\\n")
+  if k == 'RUNFILES_DIR':
+    dbg_dir=v.split("execroot")
+    env_lines.append("set directories :${project_path}:"+str(dbg_dir[0])+"\\n")
+
 
 f.writelines(env_lines)
 f.close()
 
-os.system("emacs --eval \\\"(progn (gud-gdb \\\\\\\"gdb -x ${generated_path}.cmd  --fullname ${binary} --args ${test_args}\\\\\\\") (find-file\\\\\\\"${source_path}\\\\\\\") (split-window-right) ) \\\"")
+os.system("emacs --eval \\\"(progn (gud-gdb \\\\\\\"gdb -x ${generated_path}.cmd  --fullname ${binary} --args ${test_args}\\\\\\\") (find-file \\\\\\\"${source_path}\\\\\\\") (split-window-right) ) \\\"")
 """)
 
 if __name__ == '__main__':
-  gdb = sys.argv[1]
-  generated_path = sys.argv[2]
-  test_args = sys.argv[3:]
+  project_path = sys.argv[1]
+  gdb = sys.argv[2]
+  generated_path = sys.argv[3]
+  test_args = sys.argv[4:]
   test_args[0] = os.path.abspath(test_args[0])
   test_name = test_args[0].split("/envoy/test")
-  source_path = "/git/work/fdio/envoy/test" + test_name[-1] + ".cc"
+  source_path =  str(project_path) + "/test" + test_name[-1] + ".cc"
   with open(generated_path, 'w') as f:
     f.write(
         GDB_RUNNER_SCRIPT.substitute(
             b64env=str(dict(os.environ)),
             generated_path=generated_path,
+            project_path=project_path,
             source_path=source_path,
             binary=test_args[0],
             test_args=' '.join(pipes.quote(arg) for arg in test_args)))
